c266ee6c2d9f6825ce0754ebbf3b0db3f8e8525c
guymalka <guy.malka@gmail.com>
2019-03-25 17:07:55 +0200

    secound

---
+++ build/precache-manifest.931d8dbd0a2ad0d2d26c67383e65389d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "24f6acc5fa894fe28c0e",
+    "url": "/static/js/main.4974581e.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "24f6acc5fa894fe28c0e",
+    "url": "/static/css/main.9ce82e99.chunk.css"
+  },
+  {
+    "revision": "383a2c14b889c939a2107b900e9ef244",
+    "url": "/index.html"
+  }
+];
